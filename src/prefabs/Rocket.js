c86974b6979086a5bf0a6c0b7b0dca84e1dd4e6c
Junle Yuan <76648388+JunleYuan@users.noreply.github.com>
2022-04-11 20:54:00 -0700

    Add files via upload

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -18,14 +18,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         console.log(this.cur_time/1000);
         this.cur_time++;
 
-        if(!this.isFiring) {
-            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
-                this.x -= this.moveSpeed;
-            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
-                this.x += this.moveSpeed;
-            }
-            
-        }
+        
         // fire button
         if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring && this.canFire) {
             this.isFiring = true;
